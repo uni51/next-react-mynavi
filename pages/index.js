14fae0bcfb23f0e1764e90a706bc8e7ca97e998d
Koichi.Shimomura <unieye51@gmail.com>
2022-08-28 10:53:01 +0900

    2-4　絶対パスでインポートできるようにする

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React from "react";
+import Header from "components/header";
+import Hero from "components/hero";
+import Footer from "components/footer";
 
 export default function Home() {
-  return <h1>CUBE</h1>;
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
+    </>
+  );
 }
