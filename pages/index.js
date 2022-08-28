53a4807cea43d394e0c905541cb1be0f844525bd
Koichi.Shimomura <unieye51@gmail.com>
2022-08-28 11:01:50 +0900

    2-6 カスタムAppコンポーネントでLayoutを全ページに入れる

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import Layout from "components/layout";
 import Hero from "components/hero";
 
 export default function Home() {
   return (
     <>
-      <Layout>
-        <Hero />
-      </Layout>
+      <Hero />
     </>
   );
 }
