adcdbfef2d6cf8cf799c3802d47a438b7178c6f7
Koichi.Shimomura <unieye51@gmail.com>
2022-08-31 15:07:23 +0900

    2-10 next/linkのLinkコンポーネントでページ間のリンクを設定する

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Hero from "components/hero";
 export default function Home() {
   return (
     <>
-      <Hero title="CUBE" subtitle="アウトプットしていくサイト" />
+      <Hero title="CUBE" subtitle="アウトプットしていくサイト" imageOn />
     </>
   );
 }
