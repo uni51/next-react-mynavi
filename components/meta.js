28079060d7e5633cba4189d033b0f9e58bf8097b
uni51 <unieye51@gmail.com>
2022-10-14 08:53:25 +0900

    6-4 サイトに関する情報を共有できる形で用意する

--- components/meta.js
+++ components/meta.js
@@ -1,10 +1,17 @@
 import Head from "next/head";
 
+// サイトに関する情報
+import { siteMeta } from "lib/constants";
+const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } =
+  siteMeta;
+
 export default function Meta({ pageTitle }) {
+  const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle;
+
   return (
     <Head>
-      <title>{pageTitle}</title>
-      <meta property="og:title" content={pageTitle} />
+      <title>{title}</title>
+      <meta property="og:title" content={title} />
     </Head>
   );
 }
