624679e40a92eeac881731be36a97ecd5f8d216d
uni51 <unieye51@gmail.com>
2022-10-14 08:32:47 +0900

    6-3 Metaコンポーネントでメタデータを管理する

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,4 @@
+import Meta from "components/meta";
 import Container from "components/container";
 import Hero from "components/hero";
 import PostBody from "components/post-body";
@@ -13,6 +14,8 @@ import eyecatch from "images/about.jpg";
 export default function About() {
   return (
     <Container>
+      <Meta pageTitle="アバウト" />
+
       <Hero title="About" subtitle="About development activities" />
 
       <figure>
