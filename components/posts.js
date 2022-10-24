89512ea90deec6296e968f78bd48c04f74e17025
uni51 <unieye51@gmail.com>
2022-10-24 09:11:16 +0900

    9-5 記事一覧を作成する

---
+++ components/posts.js
@@ -0,0 +1,18 @@
+import styles from "styles/posts.module.css";
+import Link from "next/link";
+
+export default function Posts({ posts }) {
+  return (
+    <div className={styles.gridContainer}>
+      {posts.map(({ title, slug }) => (
+        <article className={styles.post} key={slug}>
+          <Link href={`/blog/${slug}`}>
+            <a>
+              <h2>{title}</h2>
+            </a>
+          </Link>
+        </article>
+      ))}
+    </div>
+  );
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,27 @@
+import { getAllPosts } from "lib/api";
 import Meta from "components/meta";
 import Container from "components/container";
 import Hero from "components/hero";
+import Posts from "components/posts";
 
-export default function Blog() {
+export default function Blog({ posts }) {
   return (
     <Container>
       <Meta pageTitle="ブログ" pageDesc="ブログの記事一覧" />
+
       <Hero title="Blog" subtitle="Recent Posts" />
+
+      <Posts posts={posts} />
     </Container>
   );
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts();
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
 }
