2e716396e29c23bb3453fcdc095b48c678325ae9
shimomura-iplug <koichi.shimomura@i-plug.co.jp>
2022-10-25 17:21:10 +0900

    9-11 カテゴリページにメタデータを追加する

--- pages/blog/category/[slug].js
+++ pages/blog/category/[slug].js
@@ -1,4 +1,5 @@
 import { getAllCategories, getAllPostsByCategory } from "lib/api";
+import Meta from "components/meta";
 import Container from "components/container";
 import PostHeader from "components/post-header";
 import Posts from "components/posts";
@@ -9,6 +10,7 @@ import { eyecatchLocal } from "lib/constants";
 export default function Category({ name, posts }) {
   return (
     <Container>
+      <Meta pageTitle={name} pageDesc={`${name}に関する記事`} />
       <PostHeader title={name} subtitle="Blog Category" />
       <Posts posts={posts} />
     </Container>
